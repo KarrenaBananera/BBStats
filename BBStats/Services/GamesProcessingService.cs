5ad33939ee5514eb70d2e1ba60cd19a535a53a1b
Danila Kaslana <987r122@gmail.com>
2026-02-15 13:52:38 +0300

    fixing critical error in game service (if something will happen entire application will be down) now under try catch

--- BBStats/Services/GamesProcessingService.cs
+++ BBStats/Services/GamesProcessingService.cs
@@ -1,33 +1,44 @@
 ﻿using BBStats.Data;
+using System.Linq.Expressions;
 
 namespace BBStats.Services;
 
 public class GamesProcessingService : BackgroundService
 {
 	private readonly IServiceScopeFactory _scopeFactory;
+	private readonly ILogger<GamesProcessingService> _logger;
 
-	public GamesProcessingService(IServiceScopeFactory scopeFactory)
+	public GamesProcessingService(IServiceScopeFactory scopeFactory, ILogger<GamesProcessingService> logger)
 	{
 		_scopeFactory = scopeFactory;
+		_logger = logger;
 	}
 
 	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 	{
 		while (!stoppingToken.IsCancellationRequested)
 		{
-			using (var scope = _scopeFactory.CreateScope())
+			try
 			{
-				var client = scope.ServiceProvider.GetRequiredService<GamesFetcherClient>();
-				var parser = scope.ServiceProvider.GetRequiredService<IGamesParser>();
-				var repositroy = scope.ServiceProvider.GetRequiredService<IGamesRepository>();
-				var unparsedGames = await client.GetGames();
-
-				var games = parser.Parse(unparsedGames).OrderBy(x => x.PlayedAt);
-
-				foreach (var game in games)
+				using (var scope = _scopeFactory.CreateScope())
 				{
-					await repositroy.AddGameAsync(game);
+					var client = scope.ServiceProvider.GetRequiredService<GamesFetcherClient>();
+					var parser = scope.ServiceProvider.GetRequiredService<IGamesParser>();
+					var repositroy = scope.ServiceProvider.GetRequiredService<IGamesRepository>();
+
+					var unparsedGames = await client.GetGames();
+
+					var games = parser.Parse(unparsedGames).OrderBy(x => x.PlayedAt);
+
+					foreach (var game in games)
+					{
+						await repositroy.AddGameAsync(game);
+					}
 				}
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "Fetching games error");
 			}
 			await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
 		}
